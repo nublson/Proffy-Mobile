65b22fde5986ce318411326f6ecead57dc9cd114
Nubelson Fernandes <nubelsondev@hotmail.com>
2020-08-11 17:39:52 +0100

    criação e estilização do Favorites page

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
 
 import styles from './styles';
 
 const Favorites = () => {
-	return <View />;
+	return (
+		<View style={styles.container}>
+			<PageHeader title='Meus Proffys Favoritos' />
+
+			<ScrollView
+				style={styles.teacherList}
+				contentContainerStyle={{
+					paddingHorizontal: 16,
+					paddingBottom: 16,
+				}}
+			>
+				<TeacherItem />
+				<TeacherItem />
+			</ScrollView>
+		</View>
+	);
 };
 
 export default Favorites;
--- src/pages/Favorites/styles.ts
+++ src/pages/Favorites/styles.ts
@@ -1,5 +1,13 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#f0f0f0',
+	},
+	teacherList: {
+		marginTop: -40,
+	},
+});
 
 export default styles;
